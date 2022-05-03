d3095e97839bc288cff91ecd04caa75739e8a83f
이수빈 <snb0768@naver.com>
2022-05-03 17:39:41 +0900

    [수정] router v6 link 문제 추가 해결 필요

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+function About(props) {
+
+  return (
+    <div className='about__container'>
+      <span>"About this page: I built it because because I love movies."</span>
+      <span>- George Orwell, 1984</span>
+    </div>
+  );
+}
+
+export default About;
